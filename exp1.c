2ed6d0a7319a9d60d915479ec655abf1951a385b
Ajil <94738733+ajil02@users.noreply.github.com>
2023-10-05 10:23:47 +0530

    Add files via upload

---
+++ exp1.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+#include<ctype.h>
+
+void main()
+{
+	FILE *file;
+	char ch,arr[100];
+	int c=1;
+	file=fopen("file.txt","r");
+	int i=0;
+	do 
+	{
+        	ch = fgetc(file);
+        	arr[i]=ch;
+        	printf("%c", ch);
+        	i++;
+   	
+   	}while (ch != EOF);
+ 
+	fclose(file);
+	printf("\n");
+	printf("Lexime\tToken\t\tLine_number");
+	for(int i=0;arr[i]!=EOF;i++)
+	{
+		if (arr[i]=='+' ||arr[i]=='-' ||arr[i]=='*' ||arr[i]=='/' ||arr[i]=='%' ||arr[i]=='=')
+		{
+			printf("\n%c\toperator\t%d",arr[i],c);
+		}
+		else if(isalpha(arr[i]))
+		{
+			printf("\n%c\tidentifier\t%d",arr[i],c);
+		}
+		else if(isdigit(arr[i]))
+		{
+			printf("\n%c\tidentifier\t%d",arr[i],c);
+		}
+		else if(arr[i]==';')
+		{
+			printf("\n%c\tpunctuation\t%d",arr[i],c);
+		}
+		else if(arr[i]=='\n')
+		{
+			c++;
+		}
+	}
+}
